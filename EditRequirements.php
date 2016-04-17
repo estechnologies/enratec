e5d7ae5ea186c7499b0d3c11bed6f707f220206f
ethender <ethender@hotmail.com>
2016-04-17 17:00:09 +0530

    intial

---
+++ EditRequirements.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Create Requirement Enratec</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
+        rel="stylesheet">
+<link href="css/font-awesome.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+	<script>
+	 function selectrow1() {
+var selectedValue = document.getElementById('client').value
+if (selectedValue == 'IBM'){
+
+document.getElementById('bandid').style.display='inline';
+ document.getElementById("Band").setAttribute("required","required");
+}
+if (selectedValue == 'CTS'){
+
+document.getElementById('bandid').style.display='none';
+  document.getElementById("Band").required = false;
+ 
+     }
+ }
+ function validateinterview(){
+	document.getElementById('InterviewLoc').style.display='inline';
+		document.getElementById('Interviewdat').style.display='inline';
+	document.getElementById('walkinLoc').style.display='none';
+		document.getElementById('walkindat').style.display='none';
+		 document.getElementById("InterviewLocation").setAttribute("required","required");
+		  document.getElementById("InterviewDate").setAttribute("required","required");
+		    document.getElementById("WalkinDate").required = false;
+			  document.getElementById("WalkinLocation").required = false;
+ }
+  function validatewalkin(){
+	document.getElementById('InterviewLoc').style.display='none';
+		document.getElementById('Interviewdat').style.display='none';
+	document.getElementById('walkinLoc').style.display='inline';
+		document.getElementById('walkindat').style.display='inline';
+		 document.getElementById("WalkinLocation").setAttribute("required","required");
+		  document.getElementById("WalkinDate").setAttribute("required","required");
+		    document.getElementById("InterviewDate").required = false;
+			  document.getElementById("InterviewLocation").required = false;
+ }
+	</script>
+</head>
+<body>
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
+                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="home.php">Enratec</a>
+      <div class="nav-collapse">
+        <ul class="nav pull-right">
+          
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+                            class="icon-user"></i> Account <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="profile.php">Profile</a></li>
+              <li><a href="logout.php">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="navbar-search pull-right">
+          <input type="text" class="search-query" placeholder="Search">
+        </form>
+      </div>
+      <!--/.nav-collapse --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /navbar-inner --> 
+</div>
+<!-- /navbar -->
+<div class="subnavbar">
+  <div class="subnavbar-inner">
+    <div class="container">
+      <ul class="mainnav">
+        <li ><a href="home.html"><i class="icon-home"></i><span>Home</span> </a> </li>
+		 <li class="active dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-list-alt"></i><span></i><span>Requirements</span> <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="CreateRequirements.php">Create Requirements</a></li>
+            <li><a href="ViewRequirements.php">View & Edit Requirements</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /subnavbar-inner --> 
+</div>
+<!-- /subnavbar -->
+<div class="main">
+  <div class="main-inner">
+    <div class="container" style="">
+      <div class="row">
+        <div class="span12">
+          <div class="widget widget-nopad">
+            <div class="widget-header"> <i class="icon-list-alt"></i>
+              <h3>Edit Requirement</h3>
+            </div>
+            <!-- /widget-header -->
+			
+            <div class="widget-content">
+              <div class="widget big-stats-container">
+                <div class="widget-content">
+				
+                  <form style="margin-top:30px" class="span6 form-horizontal">
+				  <!-- Text input-->
+					<div class="control-group">
+					  <label class="control-label" for="JobId">Job Id</label>  
+					  <div class="controls">
+					  <input id="JobId" name="JobId" type="text" placeholder="" class="form-control span4 input-md" required="">
+						
+					  </div>
+					</div>
+					<!-- Select Basic -->
+					<div class="control-group">
+					  <label class="control-label" for="client">Client</label>
+					  <div class="controls">
+						<select id="client" name="client" required onchange="selectrow1();" class="span4 form-control">
+						<option value="" disabled selected hidden>Select Client</option>
+						  <option value="IBM">IBM</option>
+						  <option value="CTS">CTS</option>
+						</select>
+					  </div>
+					</div>
+
+
+					<!-- Select Basic -->
+					<div class="control-group">
+					  <label class="control-label" for="HireType">Hire Type</label>
+					  <div class="controls">
+						<select id="HireType" name="HireType" required class="span4 form-control">
+						<option value="" disabled selected hidden>Select hire type</option>
+						  <option value="Permenent">Permenent</option>
+						  <option value="Contract">Contract</option>
+						  <option value="Contract to Hire">Contract to Hire</option>
+						</select>
+					  </div>
+					</div>
+					<!-- Text input-->
+					<div class="control-group">
+					  <label class="control-label" for="Skill">Skill</label>  
+					  <div class="controls">
+						<input id="Skill" name="Skill" type="text" placeholder="" class="form-control span4  input-md" required="">
+						
+					  </div>
+					</div>
+					<!-- Text input-->
+					<div class="control-group">
+					  <label class="control-label" for="Notice">Notice</label>  
+					  <div class="controls">
+					    <input id="Notice" name="Notice" type="text" placeholder="" required class="form-control span4 input-md">
+					  </div>
+					</div>
+					<!-- Select Basic -->
+					<div id="bandid" class="control-group">
+					  <label class="control-label" for="Band">Band</label>
+					  <div class="controls">
+						<select id="Band" name="Band" class="span4 form-control">
+						<option value="" disabled selected hidden>Select Band</option>
+						  <option value="6G">6G</option>
+						  <option value="6A">6A</option>
+						  <option value="6B">6B</option>
+						  <option value="7A">7A</option>
+						  <option value="7B">7B</option>
+						  <option value="8">8</option>
+						</select>
+					  </div>
+					</div>
+					<!-- Text input-->
+					<div class="control-group">
+					  <label class="control-label" for="Experience">Experience</label>  
+					  <div class="controls">
+					  <input id="Experience" name="Experience" type="text" placeholder="" class="form-control span4 input-md" required="">
+						
+					  </div>
+					</div>
+					<!-- Textarea -->
+					<div class="control-group">
+					  <label class="control-label" for="JobDescription">Job Description</label>
+					  <div class="controls">                     
+						<textarea class="form-control span4" id="JobDescription" required name="JobDescription"></textarea>
+					  </div>
+					</div>
+					<!-- Text input-->
+					<div class="control-group">
+					  <label class="col-md-4 control-label" for="WorkingLocation">WorkingLocation</label>  
+					  <div class="controls">
+					  <input id="WorkingLocation" name="WorkingLocation" type="text" placeholder="" class="form-control span4 input-md" required="">
+						
+					  </div>
+					</div>
+					<div class="control-group">											
+						<label class="control-label">Interview/Walkin</label>
+							 <div class="controls">
+                                <label class="radio inline">
+                                     <input type="radio" name="InterviewWalkin" onclick="validateinterview();" id="InterviewWalkin-0" value="Interview" >
+      Interview
+                                 </label>
+                                <label class="radio inline">
+                                     <input type="radio" name="InterviewWalkin" onclick="validatewalkin();" id="InterviewWalkin-1" value="Walkin">
+      Walkin
+                                </label>
+                            </div>	<!-- /controls -->			
+					</div> <!-- /control-group -->
+
+					<!-- Text input-->
+					<div id="InterviewLoc" >
+					<div class="control-group">
+					  <label class="control-label" for="InterviewLocation">Interview Location</label>  
+					  <div class="controls">
+					  <input id="InterviewLocation" name="InterviewLocation" type="text" placeholder="" class="form-control span4 input-md" >
+						
+					  </div>
+					</div>
+					</div>
+					<!-- Text input-->
+					<div id="walkinLoc" style="display:none;">
+					<div class="control-group">
+					  <label class="control-label" for="WalkinLocation">Walkin Location</label>  
+					  <div class="controls">
+					  <input id="WalkinLocation" name="WalkinLocation" type="text" placeholder="" class="form-control span4 input-md" >
+						
+					  </div>
+					</div>
+					</div>
+					<!-- Text input-->
+					<div id="Interviewdat">
+					<div  class="control-group">
+					  <label class="col-md-4 control-label" for="InterviewDate">Interview Date</label>  
+					  <div class="controls">
+					  <input id="InterviewDate" name="InterviewDate" type="date" placeholder="" class="form-control span4 input-md" >
+						
+					  </div>
+					</div>
+					</div>
+					<!-- Text input-->
+					<div id="walkindat" style="display:none;" >
+					<div class="control-group">
+					  <label class="col-md-4 control-label" for="WalkinDate">Walkin Date</label>  
+					  <div class="controls">
+					  <input id="WalkinDate" name="WalkinDate" type="date" placeholder="" class="form-control span4 input-md" >
+						
+					  </div>
+					</div>
+					</div>
+					<!-- Text input-->
+					<div class="control-group">
+					  <label class="col-md-4 control-label" for="RequirementAssign">Requirement Assign</label>  
+					  <div class="controls">
+					  <input id="RequirementAssign" name="RequirementAssign" type="text" placeholder="" class="form-control span4 input-md" required="">
+						
+					  </div>
+					</div>
+					<!-- Text input-->
+					<div class="control-group">
+					  <label class="col-md-4 control-label" for="TAT">TAT Date</label>  
+					  <div class="controls">
+					  <input id="TAT" name="TATDate" type="date" placeholder="" class="form-control span4 input-md" required="">
+						
+					  </div>
+					</div>
+					<!-- Button -->
+					<div class="form-group">
+					  <label class="span4 control-label" for="Submit"></label>
+					  <div class="Span4">
+						<button id="Submit" name="Submit" class="btn btn-success">Submit</button>
+					  </div>
+					</div>
+
+</form>
+
+                </div>
+                <!-- /widget-content --> 
+                
+              </div>
+            </div>
+          </div>
+          <!-- /widget -->
+          
+         </div>
+        <!-- /span12 -->
+       
+      </div>
+      <!-- /row --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /main-inner --> 
+</div>
+
+<div class="footer navbar navbar-fixed-bottom">
+  <div class="footer-inner">
+    <div class="container">
+      <div class="row">
+        <div class="span12"> &copy; 2016 <a href="index.php">Enratec Consultancy Services PVT LTD</a>. </div>
+        <!-- /span12 --> 
+      </div>
+      <!-- /row --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /footer-inner --> 
+</div>
+<!-- /footer --> 
+<!-- Le javascript
+================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+<script src="js/jquery-1.7.2.min.js"></script> 
+<script src="js/excanvas.min.js"></script> 
+<script src="js/chart.min.js" type="text/javascript"></script> 
+<script src="js/bootstrap.js"></script>
+<script language="javascript" type="text/javascript" src="js/full-calendar/fullcalendar.min.js"></script>
+ 
+<script src="js/base.js"></script> 
+
+</body>
+</html>
---
+++ ViewRequirements.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Create Requirement Enratec</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
+        rel="stylesheet">
+<link href="css/font-awesome.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+		<style>
+.jobs-section ul li .inner-wrap {
+    background: #fefefe;
+    border: 1px solid #f4f4f4;
+    padding: 10px 15px 0;
+    transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+    -webkit-transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+    -moz-transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+    -o-transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+    -ms-transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+}
+.jobs-section ul li .job-source {
+    float: right;
+    margin-left: 10px;
+    margin-top: 4px;
+}
+.jobs-section ul li h3 {
+    color: #424242;
+    height: 50px;
+    overflow: hidden;
+    font-size: 18px;
+    line-height: 25px;
+    margin-bottom: 6px;
+}
+.jobs-section ul li header {
+    margin-bottom: 15px;
+}.jobs-section ul li p.job-info {
+    height: 62px;
+    overflow: hidden;
+}.jobs-section ul li header p {
+    margin-bottom: 7px;
+    font-size: 13px;
+    line-height: 18px;
+    font-family: 'open_sanssemibold',Helvetica,Arial,sans-serif;
+    height: 19px;
+    overflow: hidden;
+}
+.jobs-section ul li p.job-info {
+    height: 62px;
+    overflow: hidden;
+}
+.jobs-section ul li p {
+    color: #bbb;
+    font-size: 12px;
+    line-height: 21px;
+}
+.jobs-section ul li .location-info {
+    border-right: 1px solid #ccc;
+    padding-right: 8px;
+    margin-right: 8px;
+}
+.jobs-section ul li footer {
+    background: #EAEAEA;
+    padding: 8px;
+    margin: 0 -15px;
+    position: relative;
+}
+.jobs-section ul li p .detail {
+    margin-right: 20px;
+    color: #bbb;
+}
+.jobs-section ul li .apply {
+    text-align: center;
+    height: 62px;
+}
+.jobs-section ul li .jb-apply-btn-sml {
+    -moz-user-select: none;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    cursor: pointer;
+    display: block;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.42857;
+    margin-bottom: 0;
+    padding: 6px 12px;
+    text-align: center;
+    vertical-align: middle;
+    white-space: nowrap;
+    position: relative;
+    background: #fff;
+    border: 1px solid #eb494f;
+    color: #eb494f;
+    text-align: center;
+    transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+    -webkit-transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+    -moz-transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+    -o-transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+    -ms-transition: all .1s cubic-bezier(0.19, 1, 0.22, 1) 0.2s;
+    outline: none;
+    width: 115px;
+    margin: 0 auto 3px;
+}
+.jobs-section ul li footer p {
+    font-size: 13px;
+    color: #999;
+    margin: 0;
+}
+.jobs-section ul li .metadata {
+    float: right;
+}
+.jobs-section ul {
+    list-style: none;
+    margin: 0 -15px;
+    padding: 0;
+}
+.jobs-section ul li {
+    margin-bottom: 30px;
+    text-align: left;
+}
+.jobs-section ul li footer p {
+    font-size: 13px;
+    color: #999;
+    margin: 0;
+}
+
+</style>
+</head>
+<body>
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
+                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="home.php">Enratec</a>
+      <div class="nav-collapse">
+        <ul class="nav pull-right">
+          
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+                            class="icon-user"></i> Account <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="profile.php">Profile</a></li>
+              <li><a href="logout.php">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="navbar-search pull-right">
+          <input type="text" class="search-query" placeholder="Search">
+        </form>
+      </div>
+      <!--/.nav-collapse --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /navbar-inner --> 
+</div>
+<!-- /navbar -->
+<div class="subnavbar">
+  <div class="subnavbar-inner">
+    <div class="container">
+      <ul class="mainnav">
+        <li ><a href="home.html"><i class="icon-home"></i><span>Home</span> </a> </li>
+		 <li class="active dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-list-alt"></i><span></i><span>Requirements</span> <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="CreateRequirements.php">Create Requirements</a></li>
+            <li><a href="ViewRequirements.php">View & Edit Requirements</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /subnavbar-inner --> 
+</div>
+<!-- /subnavbar -->
+<div class="main">
+  <div class="main-inner">
+    <div class="container" style="">
+        <div id="job_list" class="jobs-section">
+<ul class="row">
+<!-- start -->
+
+
+
+		<li class="span6">
+<div class="inner-wrap">
+
+<h3>Client:/Skill:</h3>
+<header>
+<p class="clearfix"><span class="company-name">Hire type:    | Joining Location: | TAT Date:<i class="icon-calendar"></i></span></p>
+</header>
+<p class="job-info">
+<span class="location-info"><i class="icon-map-marker"></i>Interview/Walkin Location:  </span>
+<span class="detail"><i class="icon-calendar"></i> <strong>Notice: </strong></span><br>
+<span><strong>Job description:</strong></span>
+</p>
+
+<footer>
+<p class="clearfix">
+<span class="experience"><i class="icon-user"></i> Experience: 1-3&nbsp;yrs/Band:</span>
+ <span class="metadata"><i class="icon-calendar"></i> Interview/walkin Date:  Mar 31&nbsp&nbsp&nbsp&nbsp<span><a href="">Edit<span></span>
+</p>
+</footer>
+</div>
+</li>
+	
+  </ul>     
+      </div>
+      <!-- /row --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /main-inner --> 
+</div>
+
+<div class="footer navbar navbar-fixed-bottom">
+  <div class="footer-inner">
+    <div class="container">
+      <div class="row">
+        <div class="span12"> &copy; 2016 <a href="index.php">Enratec Consultancy Services PVT LTD</a>. </div>
+        <!-- /span12 --> 
+      </div>
+      <!-- /row --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /footer-inner --> 
+</div>
+<!-- /footer --> 
+<!-- Le javascript
+================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+<script src="js/jquery-1.7.2.min.js"></script> 
+<script src="js/excanvas.min.js"></script> 
+<script src="js/chart.min.js" type="text/javascript"></script> 
+<script src="js/bootstrap.js"></script>
+<script language="javascript" type="text/javascript" src="js/full-calendar/fullcalendar.min.js"></script>
+ 
+<script src="js/base.js"></script> 
+
+</body>
+</html>
